8a06ced98c91ad364b25326b7b8ecd4012620a71
ovidiu <>
2001-12-15 01:10:46 +0000

    (intern): Don't use double-checked locking as it is broken.

--- data/Symbol.java
+++ data/Symbol.java
@@ -46,19 +46,15 @@ public class Symbol extends Value implements Singleton {
         return new Symbol(str);
     }
 
-    public static Symbol intern(String str) {
-	Symbol s;
-	s=(Symbol)memo.get(str);
+    public synchronized static Symbol intern(String str)
+    {
+	Symbol s = (Symbol)memo.get(str);
 	if (s==null) {
-	    synchronized(memo) {
-		s=(Symbol)memo.get(str);
-		if (s==null) {
-		    s=new Symbol(str);
-		    memo.put(str, s);
-		}
-	    }
-	}
-	return s;
+            s=new Symbol(str);
+            memo.put(str, s);
+        }
+
+        return s;
     }
 
     public static Symbol get(String str) {
