2f29f4ee34604d7a54b46cf780ae2610ad3d439b
ovidiu <>
2002-01-02 23:30:49 +0000

    Updated the license to use MPL/GPL instead of the less restrictive BSD license.

--- modules/Regexp.java
+++ modules/Regexp.java
@@ -9,20 +9,33 @@
    Copyright (C) 2001 Ovidiu Predescu
    All rights reserved.
 
-   Permission to use, copy, modify, and distribute this software and its
-   documentation for any purpose and without fee is hereby granted, provided
-   that the above copyright notice appear in all copies and that both that
-   copyright notice and this permission notice appear in supporting
-   documentation.
+   The contents of this file are subject to the Mozilla Public License
+   Version 1.1 (the "License"); you may not use this file except in
+   compliance with the License. You may obtain a copy of the License
+   at http://www.mozilla.org/MPL/
+  
+   Software distributed under the License is distributed on an "AS IS"
+   basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+   the License for the specific language governing rights and
+   limitations under the License.
 
-   We disclaim all warranties with regard to this software, including all
-   implied warranties of merchantability and fitness, in no event shall
-   we be liable for any special, indirect or consequential damages or any
-   damages whatsoever resulting from loss of use, data or profits, whether in
-   an action of contract, negligence or other tortious action, arising out of
-   or in connection with the use or performance of this software.
-*/
+   The Original Code is the Second Interpreter of Scheme Code (SISC).
 
+   The Initial Developer of the Original Code is Scott G. Miller.
+   Portions created by Scott G. Miller are Copyright (C) 2000-2001
+   Scott G. Miller.  All Rights Reserved.
+
+   Alternatively, the contents of this file may be used under the
+   terms of the GNU General Public License Version 2 or later (the
+   "GPL"), in which case the provisions of the GPL are applicable
+   instead of those above.  If you wish to allow use of your version
+   of this file only under the terms of the GPL and not to allow
+   others to use your version of this file under the MPL, indicate
+   your decision by deleting the provisions above and replace them
+   with the notice and other provisions required by the GPL.  If you
+   do not delete the provisions above, a recipient may use your
+   version of this file under either the MPL or the GPL.
+ */
 package sisc.modules;
 
 import java.io.DataInputStream;
