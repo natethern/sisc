54fd9987e7c42615fee78d5cb2d619dff6763766
mradestock <>
2002-04-26 20:39:37 +0000

    fixed env var inconsistencies

--- dist/sisc.bat
+++ dist/sisc.bat
@@ -1,5 +1,5 @@
 @echo off
 set SISC_HOME="."
 set EXTENSIONS=""
-java %JAVAOPT% -Dsisc.home=%SISC_HOME% -Dsisc.heapfile=%LIBDIR%\sisc.heap -jar %SISC_HOME%\sisc.jar %EXTENSIONS% %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %JAVAOPT% -Dsisc.home=%SISC_HOME% -Dsisc.heapfile=%SISC_HOME%\sisc.heap -jar %SISC_HOME%\sisc.jar %EXTENSIONS% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
