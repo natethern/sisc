beb71b145e9c0b46d6974844e771c98ca9457843
mradestock <>
2002-05-03 00:43:07 +0000

    just some tidying up. Improves performance by a staggering 0.07%

--- LexicalEnvironment.java
+++ LexicalEnvironment.java
@@ -48,27 +48,33 @@ public class LexicalEnvironment extends Value {
     public LexicalEnvironment(Interpreter r, Closure c) 
 	throws ContinuationException {
         parent=c.env;
-        int s=c.fcount;
         Value[] v=r.vlr;
-        if (c.arity) {
-            int sm1=s-1;
-            if (v.length < sm1) 
-                error(r, liMessage(SISCB,"notenoughargstoinf", c.write(), sm1, 
-                                   v.length));
-            
-            if (v.length>=s) {
+        int vl=v.length;
+        if (!c.arity) {
+            if (vl == c.fcount) {
                 vals=v;
-            } else {
-                vals=r.createValues(s);
-                System.arraycopy(v, 0, vals, 0, sm1);
+                return;
             }
-            vals[sm1]=valArrayToList(v, sm1, v.length-sm1);
-        } else {
-            vals=v;
-            if (vals.length!=s) {
-                error(r, liMessage(SISCB,"notenoughargsto", c.write(), s, v.length));
-            }
+            error(r, liMessage(SISCB,"notenoughargsto", c.write(),
+                               c.fcount, vl));
+            return;
         }
+        
+        int sm1=c.fcount-1;
+        if (vl < sm1) {
+            error(r, liMessage(SISCB,"notenoughargstoinf", c.write(),
+                               sm1, vl));
+            return;
+        }
+
+        if (vl > sm1) {
+            vals=v;
+        } else {
+            vals=r.createValues(sm1+1);
+            System.arraycopy(v, 0, vals, 0, sm1);
+        }
+
+        vals[sm1]=valArrayToList(v, sm1, vl-sm1);
     }
 
     public final Value lookup(int depth, int pos) {
