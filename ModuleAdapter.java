aa3ea5e55f5f61e11b1f19e5dbcc5fb89649453e
mradestock <>
2002-05-09 07:36:39 +0000

    using a HashMap instead of a TreeMap allows us to get rid of Symbol.compareTo

--- ModuleAdapter.java
+++ ModuleAdapter.java
@@ -5,34 +5,34 @@ import java.util.*;
 import sisc.data.*;
 
 public abstract class ModuleAdapter extends Module {
-    protected TreeMap bindings=new TreeMap();
+    protected HashMap bindings=new HashMap(0);
 
     protected void define(String s, int id) {
         Symbol name=Symbol.get(s);
-	bindings.put(name, new Integer(id));
+        bindings.put(name, new Integer(id));
     }
 
     public Symbol[] getModuleBindingNames(Interpreter r) {
-	Symbol[] slist=new Symbol[bindings.size()];
-	int x=0;
-	for (Iterator i=bindings.keySet().iterator(); 
-	     i.hasNext();) 
-	    slist[x++]=(Symbol)i.next();
-	return slist;
+        Symbol[] slist=new Symbol[bindings.size()];
+        int x=0;
+        for (Iterator i=bindings.keySet().iterator(); 
+             i.hasNext();) 
+            slist[x++]=(Symbol)i.next();
+        return slist;
     }
 
 
     public Value getBindingValue(Interpreter r, Symbol name) throws NoSuchMethodError {
-	Integer i=(Integer)bindings.get(name);
-	try {
-	    int inum=i.intValue();
-	    return new BuiltinProcedure(this, name, inum);
-	} catch (NullPointerException n) {
-	    throw new NoSuchMethodError();
-	}
+        Integer i=(Integer)bindings.get(name);
+        try {
+            int inum=i.intValue();
+            return new BuiltinProcedure(this, name, inum);
+        } catch (NullPointerException n) {
+            throw new NoSuchMethodError();
+        }
     }
-
+    
     public float getModuleVersion() {
-	return 0.0f;
+        return 0.0f;
     }
 }
