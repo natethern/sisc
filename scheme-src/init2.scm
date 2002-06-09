beb6b1a17bd32f6d37d14b02c8b6b37749783bad
mradestock <>
2002-06-09 19:13:07 +0000

    some cleanup of new error handling code

--- scheme-src/init2.scm
+++ scheme-src/init2.scm
@@ -216,7 +216,6 @@
 ;;; Robert Hieb & Kent Dybvig
 ;;; 92/06/18
 
-
 ;;; simple delay and force; also defines make-promise
 
 (define-syntax delay
@@ -361,6 +360,53 @@
   (do ((count (- count 1) (- count 1))
        (ans '() (cons count ans)))
       ((< count 0) ans)))
+
+
+;;;;;;;;;;;;;;;; error handling ;;;;;;;;;;;;;;;
+
+;; Most of the code here is for providing SRFI-23 style error
+;; producing, which is then applied into SISC's failure-continuation
+;; model.
+
+(define (error . args)
+  (throw (apply make-error args)))
+
+(define (throw error . args)
+  (call-with-failure-continuation
+      (lambda (fk)
+        (if (null? args)
+            (call-with-current-continuation (lambda (k) (fk error k)))
+            (fk error (car args))))))
+
+(define (make-nested-error error-record parent-record)
+  (cons (cons 'parent parent-record) error-record))
+
+(define (make-error . args)
+  (let ([error-record '()])
+    ;;Location
+    (cond [(null? args) (void)]
+          [(and (not (null? args))
+                (symbol? (car args)))
+           (set! error-record (cons (cons 'location (car args)) 
+                                    error-record))
+           (set! args (cdr args))]
+          [(not (car args))
+           (set! args (cdr args))])
+      
+    ;;Message/Value
+    (let ([message (and (not (null? args))
+                        (car args))])
+      (if message (set! args (cdr args)))
+      (if (null? args)
+          (if message (set! error-record (cons `(message . ,message)
+                                               error-record)))
+          (if (string? message)
+              (set! error-record
+                (cons `(message . ,(apply format (cons message args)))
+                      error-record))
+              (error 'error "cannot specify arguments to a non format-string error."))))
+    error-record))
+
 
 ;;;;;;;;;;;;; legacy macro support ;;;;;;;;;;;;
 
