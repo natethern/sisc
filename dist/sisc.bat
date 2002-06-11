e0b9dcf4b00577bb4ffc26338b710c99fb2c3b8b
scgmille <>
2002-06-11 20:53:20 +0000

    Updated ChanegLog, startup scripts

--- dist/sisc.bat
+++ dist/sisc.bat
@@ -3,5 +3,5 @@ if NOT "%SISC_HOME%" == "" GOTO NOSET
 set SISC_HOME=.
 :NOSET
 set EXTENSIONS=
-java %JAVAOPT% -classpath %SISC_HOME%/sisc-opt.jar;%SISC_HOME%/sisc.jar;%CLASSPATH% -Dsisc.home=%SISC_HOME% -Dsisc.heapfile=%SISC_HOME%\sisc.heap sisc.REPL %EXTENSIONS% %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %JAVAOPT% -classpath %SISC_HOME%/sisc-opt.jar;%SISC_HOME%/sisc.jar;%CLASSPATH% -Dsisc.home=%SISC_HOME% -Dsisc.heapfile=%SISC_HOME%\sisc.shp sisc.REPL %EXTENSIONS% %1 %2 %3 %4 %5 %6 %7 %8 %9
 
