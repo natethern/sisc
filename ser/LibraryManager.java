8c27fe3d5325451b5c635b8825bc7a0d70b7ba3b
scgmille <>
2002-06-27 14:49:42 +0000

    Added missing

---
+++ ser/LibraryManager.java
@@ -0,0 +1,95 @@
+package sisc.ser;
+
+import sisc.data.Expression;
+import sisc.data.Symbol;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.*;
+import java.util.*;
+
+public class LibraryManager {
+    
+    protected static LibraryManager systemLibraryManager;
+
+    public static LibraryManager getInstance() {
+        if (systemLibraryManager == null)
+            systemLibraryManager = new LibraryManager();
+        return systemLibraryManager;
+    }
+
+    static class LoadableLibrary {
+        URL path;
+        Library handle;
+
+        public LoadableLibrary(URL path) {
+            this.path=path;
+        }
+
+        public LoadableLibrary(Library l) {
+            this.handle=l;
+        }
+
+        public void open() throws IOException {
+            if (handle==null) {
+                SeekableDataInputStream sis;
+                if (path.getProtocol().equals("file")) {
+                    sis=new SeekableDataInputStream(
+                         new BufferedRandomAccessInputStream(
+                          new File(new File(path.getPath()), 
+                                   path.getFile()).getCanonicalPath(),
+                          "r", 8, 2048));
+                } else {
+                    sis=new SeekableDataInputStream(new MemoryRandomAccessInputStream(path.openStream()));
+                }
+                try {
+                    handle=Library.load(sis);
+                } catch (ClassNotFoundException cnf) {
+                    cnf.printStackTrace();
+                }
+            }
+        }
+
+        public Library getLibrary() throws IOException {
+            open();
+            return handle;
+        }
+    }
+    
+    protected Map loadedLibraries;
+
+    public LibraryManager() {
+        loadedLibraries=new HashMap();
+    }
+
+    public Expression getExpression(Symbol name) throws IOException {
+        for (Iterator i=loadedLibraries.keySet().iterator(); i.hasNext();) {
+            Object key=i.next();
+            LoadableLibrary ll=(LoadableLibrary)loadedLibraries.get(key);
+            Expression rv=ll.getLibrary().getLocalExpression(name);
+            if (rv != null)
+                return rv;
+        }
+        return null;
+    }
+
+    public void addLibrary(Library l) {
+        loadedLibraries.put(l.getName(), new LoadableLibrary(l));
+    }
+
+
+    public void addLibrary(String name, URL l) {
+        loadedLibraries.put(name, new LoadableLibrary(l));
+    }
+        
+    public boolean loadLibrary(String name) {
+        //Should eventually utilize a load path
+        try {
+            URL u=new URL(name);
+            addLibrary(name, u);
+            return true;
+        } catch (MalformedURLException mfu) {
+            mfu.printStackTrace();
+        } 
+        return false;
+    }
+}
