ced836f4ee713a93fd5362872794209909c0b213
scgmille <>
2002-09-24 05:23:26 +0000

    Load the heap from a compressed (gz) file

--- contrib/applet/java/sisc/contrib/applet/SISCApplet.java
+++ contrib/applet/java/sisc/contrib/applet/SISCApplet.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.*;
+import java.util.zip.GZIPInputStream;
 
 import sisc.*;
 import sisc.ser.*;
@@ -19,7 +20,7 @@ public class SISCApplet extends JApplet {
             AppContext ctx = new AppContext();
             Context.register("main", ctx);
             Interpreter r = Context.enter("main");
-            SeekableInputStream in=new MemoryRandomAccessInputStream(conn.getInputStream());
+            SeekableInputStream in=new MemoryRandomAccessInputStream(new GZIPInputStream(conn.getInputStream()));
             REPL.initializeInterpreter(r, new String[0], in);
             Context.exit();
             SISCFrame sf=new SISCFrame();
