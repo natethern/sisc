9baae524e63b34d244354b4e6962bafe28a7e1f7
scgmille <>
2002-10-25 17:19:27 +0000

    Added check-arg, which was missing

--- scheme-src/srfi/srfi-1.scm
+++ scheme-src/srfi/srfi-1.scm
@@ -208,6 +208,9 @@
 ;;;
 ;;; The SRFI discussion record contains more discussion on this topic.
 
+(define (check-arg pred val caller)
+      (let lp ((val val))
+        (if (pred val) val (lp (error "Bad argument" val pred caller)))))
 
 ;;; Constructors
 ;;;;;;;;;;;;;;;;
