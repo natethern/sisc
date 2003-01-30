a08d4c758df6f9bf71d9eb126d462da37bfc6bfa
turadg <>
2003-01-30 02:31:32 +0000

    Move back to the 1.2 URLEncoder.encode(String)

--- modules/io/FileManipulation.java
+++ modules/io/FileManipulation.java
@@ -43,13 +43,8 @@ public class FileManipulation extends ModuleAdapter {
         URL u=url(o);
         if (!"file".equals(u.getProtocol()))
             throwPrimException(liMessage(IO.IOB, "notafileurl"));
-        try {
-            String path=URLDecoder.decode(u.getPath(), "UTF-8");
-            return new File(path);
-        } catch (UnsupportedEncodingException e) {
-            //Cannot happen
-            return null;
-        }
+	String path=URLDecoder.decode(u.getPath());
+	return new File(path);
     }
 
     public Value eval(int primid, Interpreter f)
