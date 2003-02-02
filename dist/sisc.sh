5f7e10146ff4d94c7f5ca829994e6734e1f45bd9
mradestock <>
2003-02-02 17:41:28 +0000

    added sisc-lib.jar to releases

--- dist/sisc.sh
+++ dist/sisc.sh
@@ -5,4 +5,4 @@ then
 fi
 
 EXTENSIONS=""
-java $JAVAOPT -classpath $SISC_HOME/sisc-opt.jar:$SISC_HOME/sisc.jar:$CLASSPATH -Dsisc.home=$SISC_HOME -Dsisc.heapfile=$SISC_HOME/sisc.shp sisc.REPL $EXTENSIONS "$@"
+java $JAVAOPT -classpath $SISC_HOME/sisc-opt.jar:$SISC_HOME/sisc.jar:$SISC_HOME/sisc-lib.jar $CLASSPATH -Dsisc.home=$SISC_HOME -Dsisc.heapfile=$SISC_HOME/sisc.shp sisc.REPL $EXTENSIONS "$@"
