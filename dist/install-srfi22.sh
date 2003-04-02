e43e0cd66370827d7f1a0354ab1a160b38ec4553
scgmille <>
2003-04-02 18:02:33 +0000

    Reworked the srfi-22 install mechanism

---
+++ dist/install-srfi22.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  USERID=`id -u`
+  if [ $USERID != 0 ]
+  then
+    echo This script must be run as root if installing to the default path.
+    exit 1
+  fi
+  INSTALL_DIR=/usr/local/bin
+else
+  INSTALL_DIR=$1
+fi
+
+echo This script will install the srfi-22 support scripts in $INSTALL_DIR.
+echo If this is acceptable, press enter now, otherwise press CTL-C.
+echo ""
+echo "You can call this script with a different install dir as an argument."
+read
+
+pushd .
+cd scheme-src/srfi-22
+cp srfi-22.sh $INSTALL_DIR
+cd $INSTALL_DIR
+ln -s srfi-22.sh scheme-r4rs
+ln -s srfi-22.sh scheme-r5rs
+ln -s srfi-22.sh scheme-srfi-0
+ln -s srfi-22.sh scheme-srfi-7
+ln -s srfi-22.sh scheme-ieee-1178-1900
+popd
