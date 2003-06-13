8f51e35b97d2fbfb56e5628c1dc2820b0e9a397d
mradestock <>
2003-06-13 07:37:08 +0000

    library names can be symbols

--- src/sisc/modules/libraries.scm
+++ src/sisc/modules/libraries.scm
@@ -2,25 +2,31 @@
 
 (define (locate-library lib)
   (define (suffixed-lib ext)
-    (find-resource (string-append lib "." ext)))
+    (find-resource (string-append (symbol->string lib) "." ext)))
   (or (suffixed-lib "sll")
       (suffixed-lib "sce")
       (suffixed-lib "scm")))
+
+(define (normalize-lib-name lib)
+  (if (symbol? lib) lib (string->symbol lib)))
 
 (define (require-library lib)
   (synchronized
    *libraries*
    (lambda()
-     (or (library-loaded? lib)
-         (let ([url (locate-library lib)])
-           (if (not url)
-               (error "library ~a not found" lib))
-           (load url)
-           (hashtable/put! *libraries* (string->symbol lib) #t))))))
+     (let ([lib (normalize-lib-name lib)])
+       (or (library-loaded? lib)
+           (let ([url (locate-library lib)])
+             (if (not url)
+                 (error "library ~a not found" lib))
+             (load url)
+             (hashtable/put! *libraries* lib #t)))))))
 
 (define (library-exists? lib)
-  (or (library-loaded? lib)
-      (and (locate-library lib) #t)))
+  (let ([lib (normalize-lib-name lib)])
+    (or (library-loaded? lib)
+        (and (locate-library lib) #t))))
 
 (define (library-loaded? lib)
-  (hashtable/get *libraries* (string->symbol lib)))
+  (let ([lib (normalize-lib-name lib)])
+    (hashtable/get *libraries* lib)))
