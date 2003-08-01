7e09c4e84dc6268c7f4b738b3af20aaed154d546
mradestock <>
2003-08-01 21:00:24 +0000

    load properties from sisc.properties file in user home or SISC_HOME dir

--- dist/sisc.sh
+++ dist/sisc.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-if [ -z "$SISC_HOME" ]
+if [ ! -d "$SISC_HOME" ]
 then
    SISC_HOME="`dirname $0`" #/usr/local/lib/sisc
 fi
 
+if [ -r "$HOME/sisc.properties" ]
+then
+  PROPERTIES="-p $HOME/sisc.properties"
+elif [-r "$SISC_HOME/sisc.properties" ]
+then
+  PROPERTIES="-p $SISC_HOME/sisc.properties"
+fi
+
 EXTENSIONS=""
-java $JAVAOPT -classpath $SISC_HOME/sisc-opt.jar:$SISC_HOME/sisc.jar:$SISC_HOME/sisc-lib.jar:$CLASSPATH -Dsisc.home=$SISC_HOME sisc.REPL $EXTENSIONS -h $SISC_HOME/sisc.shp "$@"
+
+java $JAVAOPT -classpath $SISC_HOME/sisc-opt.jar:$SISC_HOME/sisc.jar:$SISC_HOME/sisc-lib.jar:$CLASSPATH -Dsisc.home=$SISC_HOME sisc.REPL -h $SISC_HOME/sisc.shp $PROPERTIES $EXTENSIONS "$@"
