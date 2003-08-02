73c78ad5f4bd3ebaeccb8f7cc246bd12e36a3426
mradestock <>
2003-08-02 17:48:48 +0000

    updated to use new s2j

--- tests/s2j.scm
+++ tests/s2j.scm
@@ -1,205 +1,178 @@
-(import old-s2j)
+(import s2j)
+(import type-system)
 (import s2j-reflection)
-(import old-generic-procedures)
 
-(define-generic value-of)
-(define-generic app (generic-java-procedure 'append)) ;;do not overwrite Scheme's append
-(define-generic to-string)
-;;test calling of constructor
-(define sb (make <jstringbuffer> (->jstring "foo")))
-;;test calling of normal method
-(app sb (->jstring "foo"))
-(app sb sb)
-;;test calling of static methods
-(value-of <jstring> (->jint 1234))
-;;test overloading of Java methods and constructors in Scheme...
-(define-method (create ((meta <jstring>) class)
-                       (<jchar> c))
-  (make <jstring> (->jarray (list c) <jchar>)))
-(make <jstring>) ;calls Java method
-(make <jstring> (->jchar #\o)) ;calls scheme method
-;;test rest arg handling
-;;Wouldn't it be nice if StringBuffer.append could take any number of
-;;arguments? Well, now it can - as long as we call it from Scheme :)
-(define-method (app (next: next-method)
-                    (<jstringbuffer> buf) . rest)
-  (for-each (lambda (x) (next-method buf x)) rest)
-  buf)
-(app sb (->jstring "foo") (->jint 1) (->jstring "bar"))
-;;test object initialization
-(define-method (create (next: next-method)
-                       ((meta <jstringbuffer>) class)
-                       . rest)
-  (next-method class))
-(define-method (initialize (<jstringbuffer> sb) . rest)
-  (apply app sb rest))
-(make <jstringbuffer> (->jstring "foo") (->jint 1) (->jstring "bar"))
-;scoping
-(let ()
-  (define-generic value-of)
-  (define-method (value-of (next: next-method)
-                           ((meta <jstring>) s)
-                           (<jchar> c))
-    (display "\nLOCAL\n")
-    (next-method s c))
-  (value-of <jstring> (->jchar #\o)))
-(value-of <jstring> (->jchar #\o))
-;;define methods on Scheme types
-(define-method (app (<list> l) . rest)
-  (apply append l rest))
-(define-method (app (<vector> v) . rest)
-  (list->vector (apply append (map vector->list (cons v rest)))))
-(define-method (app (<string> s) . rest)
-  (apply string-append s rest))
-(app '(1 2) '(3 4))
-(app '#(1 2) '#(3 4))
-(app "foo" "bar")
+;;constructor
+(define-java-class <java.lang.string-buffer>)
+(define sb (java-new <java.lang.string-buffer> (->jstring "foo")))
+
+;;method invocation
+(define-generic-java-method jappend append)
+(jappend sb (->jstring "foo"))
+(jappend sb sb)
+
+;;static method invocation
+(define-java-class <java.lang.string>)
+(define-generic-java-method value-of)
+(value-of (java-null <java.lang.string>) (->jint 1234))
 
 ;array creation and access
 (define a (java-array-new <jint> '#(2 2 2)))
-(a '#(1 1 1))
-(a '#(1 1 1) (->jint 1))
-(a '(1 1 1))
+(java-array-ref a '#(1 1 1))
+(java-array-set! a '#(1 1 1) (->jint 1))
+(java-array-ref a '(1 1 1))
 (map ->number (->list (->jarray (map ->jint (iota 10)) <jint>)))
 
-;;access to static and instance fields
+;;access to instance fields
 ;;the only reason we use sisc's symbol class for testing is that the
 ;;JDK has no classes with public instance fields and I didn't want to
 ;;create a class just for testing.
-(define <sisc.data.Symbol> (java-class "sisc.data.Symbol"))
-(define s (make <sisc.data.Symbol> (->jstring "foo")))
-(s 'symval)
-(s 'case-sensitive)
-(<sisc.data.Symbol> '|caseSensitive|)
-;;bean field access
-(define <java.io.File> (java-class "java.io.File"))
-(define file (make <java.io.File> (->jstring "/foo/bar.baz")))
-(file '(canonical-file name)) ;=> bar.baz
+(define-java-class <sisc.data.symbol>)
+(define-generic-java-field-accessor :symval)
+(define-generic-java-field-modifier :symval!)
+(define s (java-new <sisc.data.symbol> (->jstring "foo")))
+(:symval s)
+(:symval! s (->jstring "bar"))
+(:symval s)
+
+;;access to static fields
+(define-java-class <sisc.data.scheme-string>)
+(define-generic-java-field-accessor :compact-representation)
+(define-generic-java-field-modifier :compact-representation!)
+(:compact-representation (java-null <sisc.data.scheme-string>))
+(:compact-representation! (java-null <sisc.data.scheme-string>)
+                          (->jboolean #t))
+(:compact-representation (java-null <sisc.data.scheme-string>))
 
 ;;equality
-(define jstring-null (java-null <jstring>))
-(eq? jstring-null jnull) ;=> #f
-(eqv? jstring-null jnull) ;=> #f
-(equal? jstring-null jnull) ;=> #t
-(eq? <jstring> jstring-null) ;=> #f
-(eqv? <jstring> jstring-null) ;=> #f
-(equal? <jstring> jstring-null) ;=> #f
-(define <java.util.Date> (java-class "java.util.Date"))
-(define d (make <java.util.Date>))
-(define time (d '(time)))
-(define d1 (make <java.util.Date> time))
-(define d2 (make <java.util.Date> time))
+(define-java-class <java.util.date>)
+(define-generic-java-method get-time)
+(define d (java-new <java.util.date>))
+(define time (get-time d))
+(define d1 (java-new <java.util.date> time))
+(define d2 (java-new <java.util.date> time))
 (eq? d1 d2) ;=> #f
 (eqv? d1 d2) ;=> #f
 (equal? d1 d2) ;=> #t
-(eq? (java-null <jstring>) (java-null <jstring>)) ;=> may return #f
-(eqv? (java-null <jstring>) (java-null <jstring>)) ;=> #t
-(equal? (java-null <jstring>) (java-null <jstring>)) ;=> #t
+(eq? (get-time d) (get-time d)) ;=> #f
+(eqv? (get-time d) (get-time d)) ;=> #t
+(equal? (get-time d) (get-time d)) ;=> #t
 
-;;special handling of jnull
-(value-of <jstring> jnull) ;=> #<java java.lang.String null>
-(type-of jnull) ;=> <java.lang.Object>
-(instance-of? jstring-null <jstring>) ;=> #t
-(define aa (java-array-new <jstring> '#(2 2 2)))
+;;use of null
+(value-of (java-null <java.lang.string>) jnull) ;=> #<java java.lang.String null>
+(define aa (java-array-new <java.lang.string> '#(2 2 2)))
 (aa '#(0 0 0)) ;=> #<jnull java.lang.String>
+(type-of (java-null <java.lang.string>)) ;=> #<java java.lang.Class java.lang.String>
 
-;;<bot>, <top>, <jclass>
-(define-generic top-bot-test)
-(define-method (top-bot-test (<top> x)) 'top)
-(define-method (top-bot-test (<jclass> x)) 'class)
-(define-method (top-bot-test (<number> x)) 'number)
-(define-method (top-bot-test (<bot> x)) 'bot)
-(top-bot-test 'foo) ;=> 'top
-(top-bot-test <jstring>) ;=> 'class
-(top-bot-test 1) ;=> 'number
-(top-bot-test jnull) ;=> 'top
-(top-bot-test (java-null <number>)) ;=> 'number
+;;equality on null
+(define jstring-null (java-null <java.lang.string>))
+(eq? jstring-null jnull) ;=> #f
+(eqv? jstring-null jnull) ;=> #f
+(equal? jstring-null jnull) ;=> #t
 
-;;generic type conversions
-((-> <list>) a)
-((-> <number>) ((-> <jint>) 1))
-((-> <boolean>) ((-> <jboolean>) #t))
-((-> <character>) ((-> <jchar>) #\a))
-((-> <string>) ((-> <jstring>) "foo"))
-((-> <vector>) a)
-((-> <symbol>) ((-> <jstring>) 'foo))
+;;widening conversion of primitive types is reflected in the
+;;inheritance hierarchy
+(java-class-precedence-list <jbyte>)
+;;=>
+#;(#<java java.lang.Class byte>
+   #<java java.lang.Class short>
+   #<java java.lang.Class int>
+   #<java java.lang.Class long>
+   #<java java.lang.Class float>
+   #<java java.lang.Class double>)
+(java-class-precedence-list <jchar>)
+;;=>
+#;(#<java java.lang.Class char>
+   #<java java.lang.Class int>
+   #<java java.lang.Class long>
+   #<java java.lang.Class float>
+   #<java java.lang.Class double>)
 
-;;primitive types are sub-types of their java.lang classes
-(define <java.lang.Integer> (java-class "java.lang.Integer"))
-(java-assignable? <jobject> <java.lang.Integer>) ;=> #t
-(java-assignable? <jobject> <jint>) ;=> #t
-(java-assignable? <java.lang.Integer> <jint>) ;=>#t
-(java-assignable? <jint> <java.lang.Integer>) ;=>#f
-(java-assignable? <jint> <jshort>) ;=>#f
-(java-assignable? <jshort> <jint>) ;=>#f
-(java-instance? <jobject> (->jint 1)) ;=>#t
-(java-instance? <java.lang.Integer> (->jint 1)) ;=>#t
-(java-instance? <jint> (->jint 1)) ;=>#t
-(java-instance? <jint> (make <java.lang.Integer> (->jint 1))) ;=>#f
+;;primitive types do not inherit from the java.lang types
+(define-java-class <java.lang.integer>)
+(instance-of? (->jint 1) <jint>) ;=> #t
+(instance-of? (->jint 1) <java.lang.integer>) ;=> #f
+
+;;widening conversion of array classes is reflected in the inheritance
+;;hierarchy
+(define-java-class <java.util.array-list**>)
+(java-class-precedence-list <java.util.array-list**>)
+;;=>
+;#(#<java java.lang.Class java.util.ArrayList[][]>
+   #<java java.lang.Class java.util.AbstractList[][]>
+   #<java java.lang.Class java.util.AbstractCollection[][]>
+   #<java java.lang.Class java.util.List[][]>
+   #<java java.lang.Class java.util.RandomAccess[][]>
+   #<java java.lang.Class java.lang.Cloneable[][]>
+   #<java java.lang.Class java.io.Serializable[][]>
+   #<java java.lang.Class java.lang.Object[][]>
+   #<java java.lang.Class java.util.Collection[][]>
+   #<java java.lang.Class java.lang.Object[]>
+   #<java java.lang.Class java.io.Serializable[]>
+   #<java java.lang.Class java.lang.Cloneable[]>
+   #<java java.lang.Class java.lang.Object>
+   #<java java.lang.Class java.io.Serializable>
+   #<java java.lang.Class java.lang.Cloneable>)
+
+;;wrapping and unwrapping of Scheme objects
+(define a (java-array-new <java.lang.object> '#(1)))
+(java-array-set! a '#(0) (java-wrap 'foo))
+(java-unwrap (java-array-ref a '#(0))) ;=> 'foo
 
 ;;implementing Java interfaces in Scheme
-(define <java.util.Comparator> (java-class "java.util.Comparator"))
-(define <java.util.Arrays> (java-class "java.util.Arrays"))
-(define <comparator-proxy> (java-proxy-class <java.util.Comparator>))
-(define (comparator fn)
-  (make <comparator-proxy>
-        (java-proxy-method-dispatcher fn)))
-(define-java-proxy-method (compare (<procedure> fn)
-                                   (<java.util.Comparator> p)
-                                   (<jobject> obj1)
-                                   (<jobject> obj2))
-  (let ([x (java-unwrap obj1)]
-        [y (java-unwrap obj2)])
-    (->jint (cond [(fn x y) -1]
-                  [(fn y x) +1]
-                  [else 0]))))
-(define-generic sort)
+(define-java-classes
+  <java.util.comparator>
+  <java.util.arrays>)
+(define-java-proxy (comparator fn)
+  (<java.util.comparator>)
+  (define (.compare p obj1 obj2)
+    (let ([x (java-unwrap obj1)]
+          [y (java-unwrap obj2)])
+      (->jint (cond [(fn x y) -1]
+                    [(fn y x) +1]
+                    [else 0])))))
+(define-generic-java-method sort)
+(define-java-class <java.lang.object>)
 (define (list-sort fn l)
-  (let ([a (->jarray (map java-wrap l) <jobject>)])
-    (sort <java.util.Arrays> a (comparator fn))
+  (let ([a (->jarray (map java-wrap l) <java.lang.object>)])
+    (sort (java-null <java.util.arrays>) a (comparator fn))
     (map java-unwrap (->list a))))
 (list-sort < '(3 4 2 1))
 (list-sort string<? '("foo" "bar" "baz"))
 
 ;;exception handling
-(define-generic char-at)
+(define-generic-java-method char-at)
 (with/fc (lambda (m e) (print-exception (make-exception m e)))
   (lambda () (char-at (->jstring "foo") (->jint 3))))
+
 ;;throwing exceptions from within proxy
-(define <java.util.Iterator> (java-class "java.util.Iterator"))
-(define <java.lang.UnsupportedOperationException>
-  (java-class "java.lang.UnsupportedOperationException"))
-(define <java.util.NoSuchElementException>
-  (java-class "java.util.NoSuchElementException"))
-(define <iterator-proxy> (java-proxy-class <java.util.Iterator>))
-(define-class (<list-iterator>) (value #f))
-(define-method (initialize (<list-iterator> i) (<top> value))
-  (i 'value value))
-(define-java-proxy-method (has-next (<list-iterator> i)
-                                    (<java.util.Iterator> p))
-  (->jboolean (not (null? (i 'value)))))
-(define-java-proxy-method (next (<list-iterator> i)
-                                (<java.util.Iterator> p))
-  (let ([ll (i 'value)])
-    (if (null? ll)
-        (error (make <java.util.NoSuchElementException>))
-        (begin
-          (i 'value (cdr ll))
-          (java-wrap (car ll))))))
-(define-java-proxy-method (remove (<list-iterator> i)
-                                  (<java.util.Iterator> p))
-  (error (make <java.lang.UnsupportedOperationException>)))
-(define-generic has-next)
-(define-generic next)
-(define-generic remove)
-(define i (make <iterator-proxy>
-                (java-proxy-method-dispatcher
-                 (make <list-iterator> '(1 2 3)))))
+(define-java-classes
+  <java.util.iterator>
+  <java.lang.unsupported-operation-exception>
+  <java.util.no-such-element-exception>)
+(define-java-proxy (list-iterator l)
+  (<java.util.iterator>)
+  (define (has-next i)
+    (->jboolean (not (null? l))))
+  (define (next i)
+    (if (null? l)
+        (error (java-new <java.util.no-such-element-exception>))
+        (let ([res (car l)])
+          (begin (set! l (cdr l))
+                 (java-wrap res)))))
+  (define (remove i)
+    (error (java-new <java.lang.unsupported-operation-exception>))))
+(define-generic-java-methods
+  has-next
+  next
+  remove)
+(define i (list-iterator '(1 2 3)))
 (has-next i)
 (next i)
 (next i)
 (next i)
 (next i)
+
 ;;catching an exception thrown by a proxy.
 (with/fc (lambda (m e) (print-exception (make-exception m e)) #f)
   (lambda () (remove i)))
@@ -209,38 +182,70 @@
   (if (> count 0)
       (begin
         (java-array-new <jdouble> 10000)
-        (make <jstringbuffer> (->jint 10000))
+        (java-new <java.lang.string-buffer> (->jint 10000))
         (loop (- count 1)))))
 
-;awt
-(define <java.awt.Frame> (java-class "java.awt.Frame"))
-(define <java.awt.FlowLayout> (java-class "java.awt.FlowLayout"))
-(define <java.awt.Button> (java-class "java.awt.Button"))
-(define-generic set-layout)
-(define-generic add)
-(define-generic pack)
-(define-generic show)
-(define frame (make <java.awt.Frame>))
-(set-layout frame (make <java.awt.FlowLayout>))
-(for-each (lambda (s) (add frame (make <java.awt.Button> (->jstring s))))
-          '(button1 button2 button3))
-(pack frame)
-(show frame)
-
-;; class precedence list
-(define <java.util.TreeSet> (java-class "java.util.TreeSet"))
-(class-precedence-list <java.util.TreeSet>)
-;; slot names
-(class-direct-slot-names <number>)
-;; applicable methods
-(define ts (make <java.util.TreeSet>))
-(define-generic remove-all)
-(applicable-methods (generic-procedure-next remove-all)
-                    (list <java.util.TreeSet> <java.util.TreeSet>))
-
+;swing
+(define-java-classes
+  (<j-frame>            |javax.swing.JFrame|)
+  (<j-button>           |javax.swing.JButton|)
+  (<j-label>            |javax.swing.JLabel|)
+  (<j-panel>            |javax.swing.JPanel|)
+  (<border-factory>     |javax.swing.BorderFactory|)
+  (<border-layout>      |java.awt.BorderLayout|)
+  (<grid-layout>        |java.awt.GridLayout|)
+  (<key-event>          |java.awt.event.KeyEvent|)
+  <java.awt.event.action-listener>)
+(define-generic-java-methods
+  get-content-pane
+  set-text
+  set-mnemonic
+  set-label-for
+  set-layout
+  set-border
+  pack
+  add
+  show
+  add-action-listener
+  create-empty-border)
+(define-generic-java-field-accessors
+  (:CENTER |CENTER|)
+  (:VK_I |VK_I|))
+(define (set-label-text label clicks)
+  (set-text label (->jstring (string-append
+                              "Number of button clicks: "
+                              (number->string clicks)))))
+(define-java-proxy (click-counter label clicks)
+  (<java.awt.event.action-listener>)
+  (define (action-performed p e)
+    (set! clicks (+ clicks 1))
+    (set-label-text label clicks)))
+(define (create-components)
+  (let ([label  (java-new <j-label>)]
+        [button (java-new <j-button> (->jstring
+                                      "I am a Swing button!"))]
+        [pane   (java-new <j-panel>)])
+    (set-label-text label 0)
+    (set-mnemonic button (:VK_I (java-null <key-event>)))
+    (add-action-listener button (click-counter label 0)) 
+    (set-label-for label button)
+    (set-border pane (create-empty-border (java-null <border-factory>)
+                                          (->jint 30)
+                                          (->jint 30)
+                                          (->jint 10)
+                                          (->jint 30)))
+    (set-layout pane (java-new <grid-layout> (->jint 0) (->jint 1)))
+    (add pane button)
+    (add pane label)
+    pane))
+(let ([frame (java-new <j-frame> (->jstring "SwingApplication"))])
+  (add (get-content-pane frame)
+       (create-components)
+       (:CENTER (java-null <border-layout>)))
+  (pack frame)
+  (show frame))
 
 ;;check exception handling in reflection api
-
 (define (find pred l)
   (and (not (null? l))
        (if (pred (car l))
@@ -264,6 +269,7 @@
                                <jaccessible-object>))
                          (list)) ;;access to protected constructor
 (define <jurl> (java/class '|java.net.URL|))
+(define <jstring> (java/class '|java.lang.String|))
 (define make-url-from-string
   (find (lambda (c) (equal? (java/parameter-types c)
                             (list <jstring>)))
