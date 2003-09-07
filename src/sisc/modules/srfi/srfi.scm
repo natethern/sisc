bf0a1c4168a2f0f92fa99fbdb451cd6f91e39b71
mradestock <>
2003-09-07 09:04:04 +0000

    added support for srfi-42

--- src/sisc/modules/srfi/srfi.scm
+++ src/sisc/modules/srfi/srfi.scm
@@ -465,11 +465,49 @@
   (define write/ss write-with-shared-structure)
   (add-feature 'srfi-38))
 
-;Still draft
 (display "srfi-39 ")
 (module srfi-39 ()
   ; Handled natively 
   (add-feature 'srfi-39))
+
+(module srfi-42
+    ((do-ec do-ec:do ec-simplify)
+     list-ec
+     append-ec
+     string-ec
+     string-append-ec
+     vector-ec
+     vector-of-length-ec
+     sum-ec
+     product-ec
+     min-ec
+     max-ec
+     any?-ec
+     every?-ec
+     (first-ec ec-guarded-do-ec)
+     last-ec
+     fold-ec
+     fold3-ec
+     ;;the following really shouldn't be exported, but because
+     ;;omprehensions assume generators are macros in their own right
+     ;;we need to do this.
+     :do
+     :let
+     (:parallel :parallel-1)
+     (:while :while-1)
+     (:until :until-1)
+     :list
+     :string
+     (:vector ec-:vector-filter)
+     :integers
+     :range
+     :real-range
+     :char-range
+     :port
+     :dispatched
+     (: :-dispatch))
+  (import srfi-23)
+  (include "srfi-42/ec.scm"))
 
 (newline)
 
