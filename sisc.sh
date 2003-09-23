a65ac41aac57482dfe8f0b380702cbacf0e704fa
scgmille <>
2003-09-23 19:07:00 +0000

    rlwrap support

--- sisc.sh
+++ sisc.sh
@@ -12,6 +12,12 @@ then
   PROPERTIES="-p $SISC_HOME/sisc.properties"
 fi
 
+#If the rlwrap readline wrapper is in the path, enable it for SISC
+if [ -x `which rlwrap` ]
+then
+   RLWRAP=rlwrap
+fi
+
 EXTENSIONS=""
 
-java $JAVAOPT -classpath $SISC_HOME/sisc-opt.jar:$SISC_HOME/sisc.jar:$SISC_HOME/sisc-lib.jar:$CLASSPATH -Dsisc.home=$SISC_HOME sisc.REPL -h $SISC_HOME/sisc.shp $PROPERTIES $EXTENSIONS "$@"
+$RLWRAP java $JAVAOPT -classpath $SISC_HOME/sisc-opt.jar:$SISC_HOME/sisc.jar:$SISC_HOME/sisc-lib.jar:$CLASSPATH -Dsisc.home=$SISC_HOME sisc.REPL -h $SISC_HOME/sisc.shp $PROPERTIES $EXTENSIONS "$@"
