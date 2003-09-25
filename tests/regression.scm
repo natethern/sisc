8ce5983c3da4cb3ed494fea129bad61857bc3cf2
mradestock <>
2003-09-25 17:31:16 +0000

    catch&report exceptions in test case wrapper

--- tests/regression.scm
+++ tests/regression.scm
@@ -3,13 +3,15 @@
 (define-syntax should-be
   (syntax-rules ()
     ((_ test-id value expression)
-     (let ((return-value expression))
-         (if (not (equal? return-value value))
-           (for-each (lambda (v) (display v))
-                     `("Failure: " test-id ", expected '"
-                     value "', got '" ,return-value "'." #\newline))
-           (for-each (lambda (v) (display v))
-                     '("Passed: " test-id #\newline)))))))
+     (with/fc (lambda (m e)
+                (display (format "Failed: ~a, expected '~a', got exception ~a\n"
+                                 test-id value (error-message m))))
+       (lambda ()
+         (let ([return-value expression])
+           (if (not (equal? return-value value))
+               (display (format "Failed: ~a, expected '~a', got '~a'\n"
+                                test-id value return-value))
+               (display (format "Passed: ~a\n" test-id)))))))))
 
 ;;used to cause an NPE
 (should-be 807474 #f
@@ -23,15 +25,12 @@
 ;;Used to be an infinite loop
 (should-be 812557 'okay (dynamic-wind void (lambda () 'okay) void))
 
-;;Used to corrupt the global environment
+;;Used to corrupt the global environment, resulting in identity being undefined.
 (should-be 812537 3
            (begin (putprop 'identity (lambda (x) x))
-                    (with/fc (lambda (m e)
-                               (void))
-                      (lambda () 
-                        (eval '(import foobarbaz) 
-                               (scheme-report-environment 5))))
-                   (with/fc (lambda (m e) 'failed)
-                      (lambda () 
-                        (eval '(identity 3))))))
+                  (with/fc (lambda (m e) (void))
+                    (lambda () 
+                      (eval '(import foobarbaz) 
+                            (scheme-report-environment 5))))
+                  (eval '(identity 3))))
                         
