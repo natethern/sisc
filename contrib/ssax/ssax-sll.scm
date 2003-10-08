a57eb987a25cfb2b77e11e92b7a37c922e9c699e
scgmille <>
2003-10-08 22:17:17 +0000

    Converted to SCC

--- contrib/ssax/ssax-sll.scm
+++
@@ -1,5 +0,0 @@
-; Creates a SISC loadable library for SSAX
-(load "ssax.scm")
-(import compiled-libraries)
-(create-library-from-module 'ssax "ssax.sll" 'ssax 'parser-error 'ascii
-	'input-parse 'util 'miscio)
