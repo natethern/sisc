af7aaecbe952c77dcfc7992efc52f3ea105d2aaf
scgmille <>
2004-02-18 18:22:45 +0000

    Allow call-with-serial-*-port's proc to return multiple values

--- src/sisc/modules/io/serial-io.scm
+++ src/sisc/modules/io/serial-io.scm
@@ -2,16 +2,20 @@
 (define open-serial-output-file (make-io-proc _open-serial-output-file))
 
 (define (call-with-serial-input-file file proc)
-  (let* ([port (open-serial-input-file file)]
-         [result (proc port)])
-    (close-input-port port)
-    result))
+  (let ([port (open-serial-input-file file)])
+    (call-with-values
+        (lambda () (proc port))
+      (lambda returns
+        (close-input-port port)
+        (apply values returns)))))
 
 (define (call-with-serial-output-file file proc)
-  (let* ([port (open-serial-output-file file)]
-         [result (proc port)])
-    (close-output-port port)
-    result))        
+  (let ([port (open-serial-output-file file)])
+    (call-with-values
+        (lambda () (proc port))
+      (lambda returns
+        (close-output-port port)
+        (apply values returns)))))
 
 (define (with-serial-input-from-file file thunk)
   (call-with-serial-input-file file 
