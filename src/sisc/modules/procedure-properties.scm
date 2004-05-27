a19705f2fe42c94a995fa46008c46fcea8a0b481
scgmille <>
2004-05-27 02:01:17 +0000

    Remove unnecessary synchronization from procedure-props, since annotations are already synchronized.

--- src/sisc/modules/procedure-properties.scm
+++ src/sisc/modules/procedure-properties.scm
@@ -1,28 +1,21 @@
-(define (assert-proc proc thunk safe?)
-  (if (procedure? proc)
-      ((if safe? synchronized synchronized-unsafe) proc thunk)
-      (error "expected procedure")))
-
 (define (procedure-property proc key . rest)
-  (assert-proc proc
-               (lambda ()
-                 (apply annotation proc key rest))
-               #f))
+  (apply annotation proc key rest))
 
 (define (set-procedure-property! proc key val . rest)
-  (assert-proc proc
-               (lambda ()
-                 (apply set-annotation! proc key val rest))
-               #f))
+  (apply set-annotation! proc key val rest))
 
 (define (procedure-property! proc key thunk . rest)
-  (assert-proc proc
-               (lambda ()
-                 (let* ([def (list #f)]
-                        [res (annotation proc key def)])
-                   (if (eq? res def)
-                       (let ([res (thunk)])
-                         (set-annotation! proc key res)
-                         res)
-                       res)))
-               (or (null? rest) (car rest))))
+  (if (procedure? proc)
+      ((if (or (null? rest) (car rest))
+           synchronized
+           synchronized-unsafe)
+       proc
+       (lambda ()
+         (let* ([def (list #f)]
+                [res (annotation proc key def)])
+           (if (eq? res def)
+               (let ([res (thunk)])
+                 (set-annotation! proc key res)
+                 res)
+               res))))
+      (error 'procedure-property! "expected procedure")))
