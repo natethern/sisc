44ba5beafc834be26d8bd6739c2d4b848c939ecf
scgmille <>
2005-04-07 20:28:56 +0000

    Fix lint issues; add scheme-srfi-55 to the srfi-22 supported aliases

--- install-srfi22.sh
+++ install-srfi22.sh
@@ -11,6 +11,12 @@ then
 else
   INSTALL_DIR=$1
 fi
+if [ -z "$2" ]
+then
+  SRCDIR="scheme-src/srfi-22"
+else
+  SRCDIR=$2
+fi
 
 echo This script will install the srfi-22 support scripts in $INSTALL_DIR.
 echo If this is acceptable, press enter now, otherwise press CTL-C.
@@ -19,12 +25,13 @@ echo "You can call this script with a different install dir as an argument."
 read
 
 pushd .
-cd scheme-src/srfi-22
+cd $SRCDIR
 cp srfi-22.sh $INSTALL_DIR
 cd $INSTALL_DIR
 ln -s srfi-22.sh scheme-r4rs
 ln -s srfi-22.sh scheme-r5rs
 ln -s srfi-22.sh scheme-srfi-0
 ln -s srfi-22.sh scheme-srfi-7
+ln -s srfi-22.sh scheme-srfi-55
 ln -s srfi-22.sh scheme-ieee-1178-1900
 popd
