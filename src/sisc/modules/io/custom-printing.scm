5aaf580df37d96aacea0037bbe98064252fb50cd
scgmille <>
2006-09-01 18:05:46 +0000

    Add remove functions

--- src/sisc/modules/io/custom-printing.scm
+++ src/sisc/modules/io/custom-printing.scm
@@ -1,6 +1,8 @@
 (import type-system)
 
-(define (new-custom-printer-map proc-name old type proc)
+; Sure would be nice to have SRFI-1 in the main env.
+
+(define (add-to-type-map proc-name old type proc)
   (if (not (procedure? proc))
       (error proc-name "expected procedure, got '~s'." proc))
   (let loop ([ls old])
@@ -12,16 +14,30 @@
                  (cdr ls))]
           [else (loop (cdr ls))])))
 
+(define (remove-from-type-map map type)
+  (cond [(null? map) '()]
+        [(equal? (caar map) type) (cdr map)]
+        [else (cons (car map)
+                    (remove-from-type-map (cdr map) type))]))
+         
 (define (add-custom-display-for-type! type proc)
   (custom-display-type-map
-   (new-custom-printer-map 'add-custom-display-for-type!
-                           (custom-display-type-map) type proc)))
+   (add-to-type-map 'add-custom-display-for-type!
+                    (custom-display-type-map) type proc)))
 
 (define (add-custom-write-for-type! type proc)
   (custom-write-type-map
-   (new-custom-printer-map 'add-custom-write-for-type!
-                           (custom-write-type-map) type proc)))
+   (add-to-type-map 'add-custom-write-for-type!
+                    (custom-write-type-map) type proc)))
 
+(define (remove-custom-display-for-type! type)
+  (custom-display-type-map
+   (remove-from-type-map (custom-display-type-map) type)))
+
+(define (remove-custom-write-for-type! type)
+  (custom-write-type-map
+   (remove-from-type-map (custom-write-type-map) type)))
+  
 (define (with-custom-write-for-type type proc thunk)
   (parameterize ((custom-write-type-map
                   (new-custom-printer-map 'with-custom-write-for-type
